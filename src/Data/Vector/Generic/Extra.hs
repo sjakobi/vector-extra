cc2e2dc5e3bd299c421614bd90c7067a52c04cca
Simon Jakobi <simon.jakobi@gmail.com>
2015-10-02 23:59:49 +0200

    Add catMaybes, vectorToMaybe and maybeToVector

--- src/Data/Vector/Generic/Extra.hs
+++ src/Data/Vector/Generic/Extra.hs
@@ -7,12 +7,25 @@ import qualified Data.Vector.Fusion.Stream.Monadic.Extra as MStream
 import Data.Either
 import Data.Either.Extra
 import qualified Data.Vector.Fusion.Stream as Stream
-import qualified Data.Vector.Fusion.Stream.Monadic as MStream
 import Data.Vector.Generic (Vector)
 import qualified Data.Vector.Generic as G
 
 mapMaybe :: (Vector v a, Vector v b) => (a -> Maybe b) -> v a -> v b
 mapMaybe f = G.unstream . Stream.inplace (MStream.mapMaybe f) . G.stream
+
+catMaybes :: (Vector v (Maybe a), Vector v a) => v (Maybe a) -> v a
+catMaybes = mapMaybe id
+
+vectorToMaybe :: (Vector v a) => v a -> Maybe a
+vectorToMaybe v
+  | G.null v = Nothing
+  | otherwise = Just (G.head v)
+
+maybeToVector :: (Vector v a) => Maybe a -> v a
+maybeToVector mx =
+  case mx of
+    Just x -> G.singleton x
+    Nothing -> G.empty
 
 lefts :: (Vector v (Either a b), Vector v a) => v (Either a b) -> v a
 lefts = G.map (\(Left x) -> x) . G.filter isLeft
@@ -21,6 +34,6 @@ rights :: (Vector v (Either a b), Vector v b) => v (Either a b) -> v b
 rights = G.map (\(Right x) -> x) . G.filter isRight
 
 partitionEithers :: (Vector v (Either a b), Vector v a, Vector v b)
-                => v (Either a b) -> (v a, v b)
+                 => v (Either a b) -> (v a, v b)
 partitionEithers =
   (\(ls, rs) -> (G.map fromLeft ls, G.map fromRight rs)) . G.partition isLeft
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,17 +10,44 @@ main = hspec $ do
   describe "mapMaybe" $ do
     context "when provided with \"const Nothing\"" $ do
       context "and a regular Vector" $
-        it "returns \"empty\"" $
-          mapMaybe (const Nothing) (V.fromList ([1..10] :: [Int]))
-            `shouldBe` (V.empty :: V.Vector Char)
+        it "returns \"empty\"" $ property $
+          \xs -> let f :: Int -> Maybe Char
+                     f = const Nothing
+                 in mapMaybe f xs == V.empty
       context "and an unboxed Vector" $
-        it "returns \"empty\"" $
-          mapMaybe (const Nothing) (U.fromList ([1..10] :: [Int]))
-            `shouldBe` (U.empty :: U.Vector Char)
+        it "returns \"empty\"" $ property $
+          \xs -> let f :: Int -> Maybe Char
+                     f = const Nothing
+                 in mapMaybe f xs == U.empty
     context "when provided with \"\\x -> if odd x then Nothing else Just x\"" $
-      it "returns only the even elements" $
-        mapMaybe (\x -> if odd x then Nothing else Just x) (U.fromList [1..10])
-          `shouldBe` (U.fromList ([2,4..10] :: [Word]))
+      it "returns only the even elements" $ property $
+        \xs -> let f :: Word -> Maybe Word
+                   f x =
+                     if odd x
+                       then Nothing
+                       else Just x
+                   xs' = mapMaybe f xs
+               in U.all even xs'
+
+  describe "catMaybes" $ do
+    it "works" $ property $
+      \xs -> let xs' = V.map (const Nothing) xs V.++ V.map Just xs
+             in catMaybes xs' == (xs :: V.Vector Char)
+
+  describe "vectorToMaybe" $ do
+      it "is inverse to \"singleton . fromJust\"" $ property $
+        \x -> vectorToMaybe (U.singleton x) == Just (x :: ())
+      context "when provided with an empty Vector" $
+        it "returns Nothing" $
+          vectorToMaybe V.empty == (Nothing :: Maybe Bool)
+
+  describe "maybeToVector" $ do
+    context "when provided with Nothing" $
+      it "returns an empty Vector" $
+        maybeToVector Nothing == (U.empty :: U.Vector Double)
+    context "when provided with a Just a" $
+      it "returns a singleton containing the a" $ property $
+        \x -> maybeToVector (Just x) == V.singleton (x :: Int)
 
   describe "lefts" $ do
     it "is inverse to \"map Left\"" $ property $
@@ -43,3 +70,7 @@ main = hspec $ do
 instance Arbitrary a => Arbitrary (V.Vector a) where
   arbitrary = V.fromList <$> arbitrary
   shrink = fmap V.fromList . shrink . V.toList
+
+instance (U.Unbox a, Arbitrary a) => Arbitrary (U.Vector a) where
+  arbitrary = U.fromList <$> arbitrary
+  shrink = fmap U.fromList . shrink . U.toList
